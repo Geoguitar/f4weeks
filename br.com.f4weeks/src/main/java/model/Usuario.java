e6f99a8fbd06349e64291ae5e52e2c6fcd9e194a
Geovane Silveira <geovanedsilveira@gmail.com>
2026-08-18 22:14:38 -0300

    implement entidades usuario e intereção com versículos

---
+++ br.com.f4weeks/src/main/java/model/Usuario.java
@@ -0,0 +1,28 @@
+package model;
+
+import jakarta.persistence.*;
+import org.testcontainers.shaded.com.google.common.collect.Table;
+
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha; // Armazenada criptografada (BCrypt)
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<InteracaoVersiculo> interacoes;
+
+    // Getters, Setters e Construtores omitidos para brevidade
+}
