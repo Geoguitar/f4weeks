aa20d1100384078b5f7136d66b6fcc756b8e5606
Geovane Silveira <geovanedsilveira@gmail.com>
2026-08-18 23:16:42 -0300

    ajustes de depencências e 12 fixes para correção pendentes

--- br.com.f4weeks/src/main/java/repository/UsuarioRepository.java
+++ br.com.f4weeks/src/main/java/repository/UsuarioRepository.java
@@ -1,6 +1,5 @@
 package repository;
 
-import com.exemplo.pentateuco.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
