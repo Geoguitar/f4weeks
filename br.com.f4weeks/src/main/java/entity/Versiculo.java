174d8a16958ad0b6bbb9fb697629d9d350afb02b
Geovane Silveira <geovanedsilveira@gmail.com>
2026-08-18 22:18:43 -0300

    refeito a entity versiculo

--- br.com.f4weeks/src/main/java/entity/Versiculo.java
+++ br.com.f4weeks/src/main/java/entity/Versiculo.java
@@ -1,74 +1,30 @@
 package entity;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
+import jakarta.persistence.*;
 
 @Entity
+@Lombok
 @Table(name = "versiculos")
 public class Versiculo {
 
-        @JsonTypeInfo.Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        private String livro; // Gênesis, Êxodo, Levítico, Números, Deuteronômio
-        private Integer capitulo;
-        private Integer versiculo;
+    @Column(nullable = false)
+    private String livro; // Ex: Gênesis
 
-        @Column(columnDefinition = "TEXT")
-        private String texto;
+    @Column(nullable = false)
+    private Integer capitulo;
 
-        // Coluna para demarcar a qual Parashá este versículo pertence
-        @Column(name = "nome_parasha")
-        private String nomeParasha;
+    @Column(nullable = false)
+    private Integer versiculo;
 
-    public Long getId() {
-        return id;
-    }
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String texto;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Column(name = "nome_parasha", nullable = false)
+    private String nomeParasha; // Ex: Bereshit
 
-    public String getLivro() {
-        return livro;
-    }
-
-    public void setLivro(String livro) {
-        this.livro = livro;
-    }
-
-    public Integer getCapitulo() {
-        return capitulo;
-    }
-
-    public void setCapitulo(Integer capitulo) {
-        this.capitulo = capitulo;
-    }
-
-    public Integer getVersiculo() {
-        return versiculo;
-    }
-
-    public void setVersiculo(Integer versiculo) {
-        this.versiculo = versiculo;
-    }
-
-    public String getTexto() {
-        return texto;
-    }
-
-    public void setTexto(String texto) {
-        this.texto = texto;
-    }
-
-    public String getNomeParasha() {
-        return nomeParasha;
-    }
-
-    public void setNomeParasha(String nomeParasha) {
-        this.nomeParasha = nomeParasha;
-    }
-// Getters, Setters e Construtores omitidos para brevidade
-
+    // Getters, Setters e Construtores omitidos para brevidade
 }
